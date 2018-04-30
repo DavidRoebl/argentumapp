3353102a7d849da33467da11d8b306a5f94536eb
David Röbl <david.roebl@pocket-lifestyle.com>
2018-04-30 12:29:00 +0200

    fixed issues on android ios still untested

--- QRCodeTestapp/src/pages/home/home.ts
+++ QRCodeTestapp/src/pages/home/home.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 //import {BarcodeScanner} from '@ionic-native/barcode-scanner';
 
 
+@IonicPage()
 @Component({
 	selector: 'page-home',
 	templateUrl: 'home.html'
@@ -13,12 +14,13 @@ export class HomePage {
 	scannerAvailable:boolean = false;
 	scanned_text:String = "nothing scanned so far :(";
 
-	constructor(public navCtrl: NavController, private qrScanner: QRScanner) {
+	constructor(public navCtrl: NavController, private qrScanner: QRScanner, private zone: NgZone) {
 		this.qrScanner.prepare().then((status: QRScannerStatus) => {
 			if(status.authorized){
 				console.log("permission granted");
 				//camera permission was granted
 				this.scannerAvailable = true;
+				this.qrScanner.show();
 					
 			} else if (status.denied){
 				console.log("permission denied");
@@ -29,8 +31,19 @@ export class HomePage {
 			} else {
 					console.log("permission temproarily not granted");
 				//permission temporarily not granted
+				//TODO: make dialog that tells user why we need the permission,
+				// and then ask again
 			}
 		}).catch((e: any) => console.log('Error is', e));
+	}
+
+	updateScannedText(text: String){
+
+		//using NgZone seems to be a popular hack to tell angular that it needs to update the view
+		// otherwise the "auto"-update stuff does NOT work (e.g. on Android, you need to force a rebuild of the view (e.g. via turning the device) to get the new value)
+		this.zone.run(() => {	
+			this.scanned_text = text;
+		});
 	}
 
 	scan(){
@@ -38,8 +51,10 @@ export class HomePage {
 			console.log("scanning started");
 			//start scanning
 			let scanSub = this.qrScanner.scan().subscribe((text : string) => {
-				console.log('Scanned something, ', text);
-				this.scanned_text = text;
+				console.log(text);
+				console.log('Scanned something, ', text, ' ... scan successful?');
+				//this.scanned_text = text;
+				this.updateScannedText(text);
 				this.qrScanner.hide();
 				scanSub.unsubscribe();
 			});
